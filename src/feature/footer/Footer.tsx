26b73b5f7828dccfaac46c654e0fff8ee5d42551
Bailey Miller <baileymiller@live.com>
2020-09-06 15:56:21 -0400

    Updated profile picture

--- src/feature/footer/Footer.tsx
+++ src/feature/footer/Footer.tsx
@@ -6,7 +6,7 @@ const SiteFooter = () =>
 <div>
     <Divider>Thanks for scrolling this far!</Divider>
     <Link href='https://github.com/BaileyMillerSSI/Giphy-Search' target='_blank'>
-        <Avatar src='https://avatars3.githubusercontent.com/u/14271740?s=460&u=1f64b2115fcebdaa7e088cd69174a14a30fe6aee&v=4'>BaileyMillerSSI</Avatar>
+        <Avatar src='https://github.com/BaileyMillerSSi.png?size=200'>BaileyMillerSSI</Avatar>
         View this project on GitHub
     </Link>
 </div>
