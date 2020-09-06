a6a726434d12ced4457a9bf00b82d426384afe07
Bailey Miller <baileymiller@live.com>
2020-09-06 17:34:41 -0400

    Implemented native share feature

--- src/feature/imageCard/ImageCard.tsx
+++ src/feature/imageCard/ImageCard.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import { Card, Avatar, message, Tooltip } from "antd";
 import {
   CopyOutlined,
-  LikeOutlined,
-  DislikeOutlined,
+  ShareAltOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import { IGif } from "@giphy/js-types";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { State } from "../../redux/State";
+import { DispatchTypes } from "../../redux/Actions";
+import { connect } from "react-redux";
 const { Meta } = Card;
 
 const copySuccess = (text: string = "Copied to clipboard!") => {
   message.success(text);
 };
 
-const ImageCard = ({ id, title, images, user }: IGif) => {
+const mapStateToProps = (state: State) => ({
+});
+
+const mapDispatchToProps = (dispatch: DispatchTypes) => ({
+  onShareTarget: async (title: string, url: string) => {
+    if(navigator.share){
+      await navigator.share({
+        title: title,
+        text:'Checkout this GIF',
+        url: url
+      });
+    }
+  }
+});
+
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps> & IGif;
+
+const ImageCard = ({ id, title, images, user, onShareTarget }: Props) => {
   const { original } = images;
   const { url } = original;
   return (
@@ -23,8 +43,7 @@ const ImageCard = ({ id, title, images, user }: IGif) => {
       style={{ width: 300 }}
       cover={<img alt={title} src={url} height={280} width={470} />}
       actions={[
-        <LikeOutlined key="like" />,
-        <DislikeOutlined key="dislike" />,
+        <ShareAltOutlined onClick={()=> onShareTarget(title, url)} key="share-to"/>,
         <Tooltip
           title={`Copy ${
             title.length !== 0 ? title : "GIF"
@@ -33,7 +52,7 @@ const ImageCard = ({ id, title, images, user }: IGif) => {
           <CopyToClipboard text={url} onCopy={() => copySuccess()}>
             <CopyOutlined key="copy-to-clipboard" />
           </CopyToClipboard>
-        </Tooltip>,
+        </Tooltip>
       ]}
     >
       <Meta
@@ -44,4 +63,4 @@ const ImageCard = ({ id, title, images, user }: IGif) => {
   );
 };
 
-export default ImageCard;
+export default connect(mapStateToProps, mapDispatchToProps)(ImageCard);
