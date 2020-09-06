1860d76f5c9d2e68a430171e5e07312c6cb953f0
Bailey Miller <baileymiller@live.com>
2020-09-06 16:00:04 -0400

    Using the spacing component to make the footer look nicer

--- src/feature/footer/Footer.tsx
+++ src/feature/footer/Footer.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Avatar, Typography, Divider } from 'antd';
+import { Avatar, Typography, Divider, Space } from 'antd';
 const { Link } = Typography;
 
 const SiteFooter = () =>
 <div>
     <Divider>Thanks for scrolling this far!</Divider>
     <Link href='https://github.com/BaileyMillerSSI/Giphy-Search' target='_blank'>
-        <Avatar src='https://github.com/BaileyMillerSSi.png?size=200'>BaileyMillerSSI</Avatar>
-        View this project on GitHub
+        <Space>
+            <Avatar src='https://github.com/BaileyMillerSSi.png?size=200'>BaileyMillerSSI</Avatar>
+            View this project on GitHub
+        </Space>
     </Link>
 </div>
 
