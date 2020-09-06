c4a56486f95b2f1e867fbef74f0d43b817ceced8
Bailey Miller <baileymiller@live.com>
2020-09-06 17:01:22 -0400

    Fixed up some spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { Row, Col, Form, Layout } from "antd";
+import { Row, Col, Form, Layout, Space } from "antd";
 import SearchBox from "./feature/form/SearchBox";
 import RatingSelect from "./feature/form/RatingSelect";
 import Results from "./feature/results/Results";
-import './App.css'
+import "./App.css";
 import SiteFooter from "./feature/footer/Footer";
 const { Header, Content, Footer } = Layout;
 
 const App = () => (
-  <Layout>
-    <Header className="bkgd_white">
-      <Form className="form">
-        <Row gutter={16}>
-          <Col>
-            <SearchBox />
-          </Col>
-          <Col>
-            <RatingSelect />
-          </Col>
-        </Row>
-      </Form>
-    </Header>
-    <Content className="bkgd_white">
-      <Results />
-    </Content>
-    <Footer className="bkgd_white footer">
-      <SiteFooter/>
-    </Footer>
-  </Layout>
+    <Layout className="bkgd_white">
+      <Header className="bkgd_white">
+        <Form className="form">
+          <Row gutter={16} justify="center">
+            <Col>
+              <SearchBox />
+            </Col>
+            <Col>
+              <RatingSelect />
+            </Col>
+          </Row>
+        </Form>
+      </Header>
+      <Content className="bkgd_white space-top">
+        <Results />
+      </Content>
+      <Footer className="bkgd_white space-top footer">
+        <SiteFooter />
+      </Footer>
+    </Layout>
 );
 
 export default App;
